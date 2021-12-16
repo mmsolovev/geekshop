353e2f49d736b2d591696940167053c460787d44
mmsolovev <mmsolovevmail@gmail.com>
2021-12-16 18:52:39 +0300

    lesson_14. Code style fix.

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
@@ -11,7 +12,6 @@ from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
 
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 class OrderList(LoginRequiredMixin, ListView):
     model = Order
